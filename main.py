b869f4d4ac25824491dc3ed49cec2387f2c369fe
Yuki <risaton@risaton.net>
2022-12-23 00:44:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import discord
 from discord import *
 
 load_dotenv()
@@ -7,3 +8,9 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 DEEPL_KEY = os.environ.get("DEEPL_KEY")
 
 bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as: {bot.user}")
+
+bot.run(TOKEN)
